5f4d71dbb6bd45a75302e3aaeaa106cafc52f5a7
Mohamad 2007 <almohamad152007@gmail.com>
2026-09-21 17:58:52 +0300

    Update lib/models/surah.dart

---
+++ lib/models/surah.dart
@@ -0,0 +1,9 @@
+class Surah {
+  const Surah({required this.number, required this.name, required this.translation, required this.ayahs, required this.revelation});
+
+  final int number;
+  final String name;
+  final String translation;
+  final int ayahs;
+  final String revelation;
+}
